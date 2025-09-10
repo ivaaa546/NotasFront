30b65a2cb6e5a9e7720fba2585003fe803c6e742
ivan <ivanvrs@proton.me>
2025-09-09 20:20:34 -0600

    segundo commit

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -4,6 +4,9 @@ interface ImportMetaEnv {
   readonly VITE_API_BASE_URL: string
   readonly VITE_APP_NAME: string
   readonly VITE_APP_VERSION: string
+  readonly VITE_API_URL: string            // backend Render
+  readonly VITE_SUPABASE_URL: string      // Supabase
+  readonly VITE_SUPABASE_ANON_KEY: string // Supabase
 }
 
 interface ImportMeta {
